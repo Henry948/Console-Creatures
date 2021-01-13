91b099d95e833dadfd7e8695402082bb91f19535
dahdi <62443126+dahdi@users.noreply.github.com>
2021-01-13 19:23:32 +0000

    Create Program.cs
    Created program.cs and began writing code for desired output

---
+++ Program.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+        //The code below will display a string in the shape of an animal
+        //this is a project to work on supplied by code academy
+        // personal progression and reference
+      Console.WriteLine(" .-.");    
+      Console.WriteLine("(o o)");
+      Console.WriteLine("| O |");
+     }
+  }
+}
